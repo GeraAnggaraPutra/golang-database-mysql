dab74265632b5f69d65bc4e58e2f4de2d78b7add
GeraAnggaraPutra <gera_098@smkassalaambandung.sch.id>
2023-05-08 20:58:54 +0700

    database transaction

--- sql_test.go
+++ sql_test.go
@@ -234,3 +234,40 @@ func TestPrepareStatement(t *testing.T) {
 		fmt.Println("Comment Id ", id)
 	}
 }
+
+func TestTransaction(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+	tx, err :=db.Begin()
+	if err != nil {
+		panic(err)
+	}
+
+	script := "INSERT INTO comments(email, comment) VALUES(?, ?)"
+	// do transaction
+	for i := 1; i <= 10; i++ {
+		email := "anggara" + strconv.Itoa(i) + "@gmail.com"
+		comment := "Komentar ke " + strconv.Itoa(i)
+
+		result, err := tx.ExecContext(ctx, script, email, comment)
+		if err != nil {
+			panic(err)
+		}
+
+		id, err := result.LastInsertId()
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println("Comment Id ", id)
+	}
+
+	tx.Commit() // untuk mengirim data ke database
+
+	// err = tx.Rollback() // untuk membatalkan pengiriman data 
+	// if err != nil {
+	// 	panic(err)
+	// }
+}
