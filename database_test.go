eb6ad6777f0bda429275840fac383d47d8efc33a
GeraAnggaraPutra <gera_098@smkassalaambandung.sch.id>
2023-05-08 18:22:28 +0700

    first commit

---
+++ database_test.go
@@ -0,0 +1,17 @@
+package golang_database_mysql
+
+import (
+	"database/sql"
+	"testing"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func TestOpenConnection(t *testing.T) {
+	db, err := sql.Open("mysql", "root:@tcp(localhost:3306)/belajar-golang-database")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	// gunakan DB
+}
---
+++ sql_test.go
@@ -0,0 +1,106 @@
+package golang_database_mysql
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestExecSql(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	script := "INSERT INTO customer(name) VALUES('Gera')"
+	_, err := db.ExecContext(ctx, script)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Success insert new customer")
+}
+
+func TestQuerySql(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	script := "SELECT id, name FROM customer"
+	rows, err := db.QueryContext(ctx, script)
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var id int
+		var name string
+		err = rows.Scan(&id, &name)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("Id:", id)
+		fmt.Println("Name:", name)
+	}
+}
+
+func TestQuerySqlComplexInsert(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	script := "INSERT INTO customer(name, email, balance, rating, birth_date, married) VALUES('Putra', 'putra@gmail.com', 100000, 5.0, '2000-6-6', true),('Angga', 'angga@gmail.com', 100000, 5.0, '2000-6-7', true)"
+	_, err := db.ExecContext(ctx, script)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Success insert new customer")
+}
+
+func TestQuerySqlComplex(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	script := "SELECT id, name, email, balance, rating, birth_date, married, created_at FROM customer"
+	rows, err := db.QueryContext(ctx, script)
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var id, name string
+		var email sql.NullString
+		var balance int32
+		var rating float64
+		var birthDate sql.NullTime
+		var createdAt time.Time
+		var married bool
+
+		err = rows.Scan(&id, &name, &email, &balance, &rating, &birthDate, &married, &createdAt)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("================")
+		fmt.Println("Id:", id)
+		fmt.Println("Name:", name)
+		if email.Valid {
+			fmt.Println("Email:", email.String)
+		}
+		fmt.Println("Balance:", balance)
+		fmt.Println("Rating:", rating)
+		if birthDate.Valid {
+			fmt.Println("Birth Date:", birthDate.Time)
+		}
+		fmt.Println("Married:", married)
+		fmt.Println("Created At:", createdAt)
+	}
+}
